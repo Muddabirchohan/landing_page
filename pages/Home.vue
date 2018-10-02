18125906232796f92abca892f11e7b0b78725e23
Muddabirchohan <muddabir22@gmail.com>
2018-10-02 21:53:13 +0500

    2nd commit

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-
     <div
       id="top"
       class="frontimage js-cd-top">
@@ -8,7 +7,10 @@
       <div style="color: white; position: fixed">
         Window width: {{ windowWidth }}
         <br> Window height: {{ windowHeight }}
+        <br> Find Top: {{ findTop }}
       </div>
+
+
 
       <!--<b-button-->
       <!--id="buttonfade"-->
@@ -444,26 +446,35 @@
       <p class="ending"> © 2018 All Rights Reserved </p>
     </div>
 
+
+
     <!--<b-button @click="topFunction"> top </b-button>-->
 
-    <div>
+
+
+
+    <div v-if="(mb > windowHeight+200)">
       <a
-        href="#top"
-        class="cd-top js-cd-top topbutton">Top  </a>
+        class="topbutton"
+        href="#top"> Top  {{ ( mb > 1000 ) }}
+      </a>
     </div>
-
-
 
   </div>
 
 
 </template>
 
+
+
+
 <script>
+
 
   import Navbar from './Navbar.vue';
 
   export default {
+
     components: {
       Navbar
     },
@@ -472,10 +483,28 @@
         msg: 'Hello World! This is a Event listener test.',
         windowWidth: 0,
         windowHeight: 0,
+        mb: 7,
+        height: 0,
       }
+
     },
 
+
     mounted() {
+      window.onscroll = () => {
+
+        var d = document.documentElement;
+        var offset = d.scrollTop + window.innerHeight;
+        var height = d.offsetHeight;
+         this.mb = offset;
+        this.height = height;
+        console.log("offset" + this.mb);
+        console.log("windowHeight" + this.windowHeight);
+        if (offset === height) {
+          console.log('At the bottom');
+        }
+      },
+
       this.$nextTick(function() {
         window.addEventListener('resize', this.getWindowWidth);
         window.addEventListener('resize', this.getWindowHeight);
@@ -483,92 +512,9 @@
         //Init
         this.getWindowWidth()
         this.getWindowHeight()
-      }),
-
-
-
-
-        (function(){
-          // Back to Top - by CodyHouse.co
-          var backTop = document.getElementsByClassName('js-cd-top')[0],
-            // browser window scroll (in pixels) after which the "back to top" link is shown
-            offset = 300,
-            //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-            offsetOpacity = 1200,
-            scrollDuration = 700,
-            scrolling = false;
-          if( backTop ) {
-            //update back to top visibility on scrolling
-            window.addEventListener("scroll", function(event) {
-              if( !scrolling ) {
-                scrolling = true;
-                (!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
-              }
-            });
-            //smooth scroll to top
-            backTop.addEventListener('click', function(event) {
-              event.preventDefault();
-              (!window.requestAnimationFrame) ? window.scrollTo(0, 0) : scrollTop(scrollDuration);
-            });
-          }
-
-          function checkBackToTop() {
-            var windowTop = window.scrollY || document.documentElement.scrollTop;
-            ( windowTop > offset ) ? addClass(backTop, 'cd-top--show') : removeClass(backTop, 'cd-top--show', 'cd-top--fade-out');
-            ( windowTop > offsetOpacity ) && addClass(backTop, 'cd-top--fade-out');
-            scrolling = false;
-          }
-
-          function scrollTop(duration) {
-            var start = window.scrollY || document.documentElement.scrollTop,
-              currentTime = null;
-
-            var animateScroll = function(timestamp){
-              if (!currentTime) currentTime = timestamp;
-              var progress = timestamp - currentTime;
-              var val = Math.max(Math.easeInOutQuad(progress, start, -start, duration), 0);
-              window.scrollTo(0, val);
-              if(progress < duration) {
-                window.requestAnimationFrame(animateScroll);
-              }
-            };
-
-            window.requestAnimationFrame(animateScroll);
-          }
-
-          Math.easeInOutQuad = function (t, b, c, d) {
-            t /= d/2;
-            if (t < 1) return c/2*t*t + b;
-            t--;
-            return -c/2 * (t*(t-2) - 1) + b;
-          };
-
-          //class manipulations - needed if classList is not supported
-          function hasClass(el, className) {
-            if (el.classList) return el.classList.contains(className);
-            else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
-          }
-          function addClass(el, className) {
-            var classList = className.split(' ');
-            if (el.classList) el.classList.add(classList[0]);
-            else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
-            if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
-          }
-          function removeClass(el, className) {
-            var classList = className.split(' ');
-            if (el.classList) el.classList.remove(classList[0]);
-            else if(hasClass(el, classList[0])) {
-              var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
-              el.className=el.className.replace(reg, ' ');
-            }
-            if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
-          }
-        })();
-
-
-
-
+      })
     },
+
     beforeDestroy() {
       window.removeEventListener('resize', this.getWindowWidth);
       window.removeEventListener('resize', this.getWindowHeight);
@@ -580,7 +526,7 @@
           this.windowWidth = document.documentElement.clientWidth;
         },
 
-        getWindowHeight(event) {
+         getWindowHeight(event) {
           this.windowHeight = document.documentElement.clientHeight;
         },
 
@@ -591,9 +537,12 @@
         scrollSmoothToBottom: function () {
           window.scrollTo(0, 800);
         },
-      },
 
-  }
+
+
+  },
+
+    }
 </script>
 
 
@@ -614,7 +563,10 @@
     bottom: 0px;
     right: 0;
     border-radius: 50%;
+    text-align: center;
+    padding-top: 15px;
   }
+
 
   .head1{
     color: white;
